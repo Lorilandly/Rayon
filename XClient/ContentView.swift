db2fe6295c54ab2986fa7458dc01b30cbf5368b6
Lori <lihaoan1001@163.com>
2023-03-15 16:21:14 +0800

    Initial Commit

---
+++ XClient/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  XClient
+//
+//  Created by 李浩安 on 2023/3/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XClient/XClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  XClientApp.swift
+//  XClient
+//
+//  Created by 李浩安 on 2023/3/15.
+//
+
+import SwiftUI
+
+@main
+struct XClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
